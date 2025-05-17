5992bd9e116beb2c83a44517d069e48b21de54d1
Mariana Palacios <mariana.palacios@arreglatechcompany.com>
2025-05-17 13:33:18 -0500

    Update environment configuration: add PLP_ASEO_SLUG and adjust default values in env helper

--- src/helpers/env.helper.ts
+++ src/helpers/env.helper.ts
@@ -4,11 +4,12 @@ import * as dotenvExpand from "dotenv-expand";
 dotenvExpand.expand(dotenv.config({ path: ".env", override: false }));
 
 export const env = {
-  baseUrl: process.env.BASE_URL!,
-  tiendaUrl: process.env.TIENDA_URL!,
-  pdpSlug: process.env.PDP_SLUG!,
-  plpSlug: process.env.PLP_SLUG!,
-  pdpName: process.env.PDP_NAME!,
-  pdpQty: process.env.PDP_QTY_DEFAULT || "1",
-  defaultTimeout: Number(process.env.DEFAULT_TIMEOUT) || 5000,
+  baseUrl: process.env.BASE_URL || "https://www.exito.com",
+  tiendaUrl: process.env.TIENDA_URL || "https://www.exito.com/tienda",
+  pdpSlug: process.env.PDP_SLUG || "/televisor-samsung-60-uhd4k-smart-tv/p",
+  plpSlug: process.env.PLP_SLUG || "/tecnologia/televisores",
+  plpAseoSlug: process.env.PLP_ASEO_SLUG || "/mercado/aseo-del-hogar",
+  pdpName: process.env.PDP_NAME || "Televisor SAMSUNG 60″ UHD4K Smart TV",
+  pdpQty: process.env.PDP_QTY || "1",
+  defaultTimeout: Number(process.env.DEFAULT_TIMEOUT) || 7000,
 };
